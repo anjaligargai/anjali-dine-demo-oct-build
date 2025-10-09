1d3357a5c478a9245f592f9c079b4fc02332d83d
AishwaryaShejwal1234 <Aishwarya.Shejwal@bitwiseglobal.com>
2025-10-09 10:33:51 +0530

    Update evaluate.py

--- pipelines/abalone/evaluate.py
+++ pipelines/abalone/evaluate.py
@@ -1,59 +1,25 @@
-"""Evaluation script for measuring mean squared error."""
 import json
-import logging
+import os
 import pathlib
-import pickle
-import tarfile
-
-import numpy as np
 import pandas as pd
-import xgboost
-
-from sklearn.metrics import mean_squared_error
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-logger.addHandler(logging.StreamHandler())
-
+from sklearn.metrics import f1_score
 
 
 if __name__ == "__main__":
-    logger.debug("Starting evaluation.")
-    model_path = "/opt/ml/processing/model/model.tar.gz"
-    with tarfile.open(model_path) as tar:
-        tar.extractall(path=".")
-
-    logger.debug("Loading xgboost model.")
-    model = pickle.load(open("xgboost-model", "rb"))
-
-    logger.debug("Reading test data.")
-    test_path = "/opt/ml/processing/test/test.csv"
-    df = pd.read_csv(test_path, header=None)
-
-    logger.debug("Reading test data.")
-    y_test = df.iloc[:, 0].to_numpy()
-    df.drop(df.columns[0], axis=1, inplace=True)
-    X_test = xgboost.DMatrix(df.values)
-
-    logger.info("Performing predictions against test data.")
-    predictions = model.predict(X_test)
-
-    logger.debug("Calculating mean squared error.")
-    mse = mean_squared_error(y_test, predictions)
-    std = np.std(y_test - predictions)
+    y_pred_path = "/opt/ml/processing/input/predictions/x_test.csv.out"
+    y_pred = pd.read_csv(y_pred_path, header=None)
+    y_true_path = "/opt/ml/processing/input/true_labels/y_test.csv"
+    y_true = pd.read_csv(y_true_path, header=None)
     report_dict = {
-        "regression_metrics": {
-            "mse": {
-                "value": mse,
-                "standard_deviation": std
+        "classification_metrics": {
+            "weighted_f1": {
+                "value": f1_score(y_true, y_pred, average="weighted"),
+                "standard_deviation": "NaN",
             },
         },
     }
-
     output_dir = "/opt/ml/processing/evaluation"
     pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
-
-    logger.info("Writing out evaluation report with mse: %f", mse)
-    evaluation_path = f"{output_dir}/evaluation.json"
+    evaluation_path = os.path.join(output_dir, "evaluation_metrics.json")
     with open(evaluation_path, "w") as f:
         f.write(json.dumps(report_dict))
